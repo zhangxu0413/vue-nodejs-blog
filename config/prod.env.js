89af68f1f0bf8cf3aeef2651ab02bfcbfbc4fc80
zhangxu <zhangxu0413@139.com>
2019-03-14 19:53:49 +0800

    fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
-  HOST:'"http://www.shuyuxuan.cn:8088"',
-  API_ROOT:'"http://www.shuyuxuan.cn:8088/api"'
+    NODE_ENV: '"production"',
+    HOST: '"http://www.shuyuxuan.cn:8088"',
+    API_ROOT: '"http://www.shuyuxuan.cn/api"'
 }
